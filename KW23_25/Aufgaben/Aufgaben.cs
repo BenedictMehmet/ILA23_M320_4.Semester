09c728ecab06c00975fb510f688898cb24a1abc9
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-06-03 20:17:50 +0200

    add tasks to week23

---
+++ KW23_25/Aufgaben/Aufgaben.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILA23_M320_4.Semester.KW23_25.Aufgaben
+{
+    internal class Aufgaben
+    {
+        /*
+ * 1.
+ * Erstelle ein einfaches C#-Programm, das Vererbung demonstriert.
+ * Definiere eine Basisklasse Animal mit einer Methode MakeSound.
+ * Erstelle dann eine abgeleitete Klasse Dog, die die Methode MakeSound überschreibt.
+ * Verwende das Schlüsselwort base, um die Methode der Basisklasse innerhalb der Methode der abgeleiteten Klasse aufzurufen.
+ * Gib die Laute auf der Konsole aus.
+ 
+   Achtung!
+ 
+   Das Ausführungsergebnis sollte sein:
+ 
+   "Animal sound" "Dog barks"
+ 
+ 
+ 
+ * 2.
+ * Erstelle ein einfaches C#-Programm, das die Vererbung von Konstruktoren demonstriert.
+ * Definiere eine Basisklasse Person mit einem Konstruktor, der einen Namen entgegennimmt und eine Meldung ausgibt.
+ * Erstelle dann eine abgeleitete Klasse Employee, die den Konstruktor der Basisklasse aufruft und zusätzliche Funktionalität hinzufügt.
+ * Gib die Meldungen auf der Konsole aus.
+ 
+   Achtung!
+ 
+   Das Ausführungsergebnis sollte sein:
+ 
+   "Person constructor: John Doe" "Employee constructor: John Doe, ID: 123"
+ 
+ 
+ 
+ * 3.
+ * Erstelle ein C#-Programm, das das Überschreiben von Methoden mit den Schlüsselwörtern override und virtual demonstriert. Das Programm soll:
+ 
+   Eine Basisklasse Animal mit einer virtuellen Methode MakeSound deklarieren, die "Animal makes a sound" ausgibt.
+ 
+   Eine abgeleitete Klasse Dog deklarieren, die die Methode MakeSound überschreibt und "Dog barks" ausgibt.
+ 
+   Eine weitere abgeleitete Klasse Cat deklarieren, die die Methode MakeSound überschreibt und "Cat meows" ausgibt.
+ 
+   Erstelle Instanzen von Animal, Dog und Cat und rufe die Methode MakeSound für jede Instanz auf, um das Überschreiben der Methode zu demonstrieren.
+ 
+   Achtung!
+ 
+   Das Ausführungsergebnis für die Standardausgabe sollte sein:
+ 
+   "Animal makes a sound" "Dog barks" "Cat meows"
+ 
+ 
+ 
+   4.
+   Erstelle ein einfaches C#-Programm, das die Implementierung und Verwendung von Interfaces demonstriert.
+   Definiere ein Interface IVehicle mit einer Methode Drive.
+   Erstelle dann eine Klasse Car, die dieses Interface implementiert und eine Implementation für die Methode Drive bereitstellt.
+   Gib eine Meldung von der Drive-Methode auf der Konsole aus.
+ 
+   Achtung!
+ 
+   Das Ausführungsergebnis sollte sein:
+ 
+   "Car is driving"
+ 
+ 
+ *
+ */
+    }
+}
