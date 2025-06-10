e76e81be1670f6783840e63f894c2c7ea33882c5
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-06-10 16:32:47 +0200

    add more classes

---
+++ KW24_25/Tasks/Tasks.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILA23_M320_4.Semester.KW24_25.Tasks
+{
+    internal class Tasks
+    {
+        /*
+       1.
+       Create a simple C# program that demonstrates polymorphism using interfaces.
+       Define an interface IPayment with a method ProcessPayment.
+       Then, create two classes CreditCardPayment and PayPalPayment
+       that implement this interface and provide their own implementations for the ProcessPayment method.
+       Use polymorphism to call the ProcessPayment method on instances of both classes.
+
+       Alert!
+
+       The result of execution should be:
+
+       "Processing credit card payment" "Processing PayPal payment"
+
+
+
+       2.
+
+       Create a C# program that demonstrates Constructor Injection,
+       Setter Injection, and Interface Injection.
+       Implement a simple service ILoggingService and a class MyClass that depends on this service.
+       Demonstrate each type of dependency injection method.
+
+       Alert!
+
+       The result of execution should be:
+
+       Constructor Injection: Logging message.
+       Setter Injection: Logging message.
+       Interface Injection: Logging message.
+
+       3.
+
+       Please, avoid changing the given source code for the exercise! So only add code, don't modify unless it is instructed to do so!
+
+       #########################################
+
+       Create a simple C# program that demonstrates polymorphism using interfaces. The program should:
+
+       Declare an interface IShape with a method double GetArea().
+
+       Create two classes, Circle and Rectangle, that implement the IShape interface.
+
+       The Circle class should have a constructor that takes a radius and implements the GetArea method to return the area of the circle.
+
+       The Rectangle class should have a constructor that takes width and height, and implements the GetArea method to return the area of the rectangle.
+
+       In the PrintAreas method, create an array of IShape containing instances of Circle and Rectangle, and print the area of each shape to the console.
+
+       Alert!
+
+       The result of execution should be the area of each shape printed to the console.
+
+       Example:
+
+       For a Circle with radius 5 and a Rectangle with width 4 and height 6, the output should be:
+
+       Area: 78.53981633974483\nArea: 24\n
+
+       */
+    }
+}
