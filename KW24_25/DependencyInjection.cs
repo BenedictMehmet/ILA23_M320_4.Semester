46572de1217276106daf60a50d78d8422ee4cb6d
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-06-11 10:58:16 +0200

    add more classes and tasks

---
+++ KW24_25/DependencyInjection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILA23_M320_4.Semester.KW24_25
+{
+    internal class DependencyInjection
+    {
+        static void Main(string[]args)
+        {
+            Hammer h1 = new Hammer();
+            Sagen s1 = new Sagen();
+
+            Bauen b1 = new Bauen(h1, s1);
+            b1.HausBauen();
+        }
+    }
+
+
+
+
+    public class Hammer
+    {
+        public void Use()
+        {
+            Console.WriteLine("Nägel einschlagen");
+        }
+    }
+
+
+    public class Sagen
+    {
+        public void Use()
+        {
+            Console.WriteLine("Holz sägen");
+        }
+    }
+
+
+
+    public class Bauen
+    {
+        private Hammer _hammer;
+        private Sagen _sagen;
+
+
+        public Bauen(Hammer hammer, Sagen sagen)
+        {
+            this._hammer = hammer;
+            this._sagen = sagen;
+        }
+
+
+        public void HausBauen()
+        {
+            _hammer.Use();
+            _sagen.Use();
+            Console.WriteLine("Haus gebaut");
+        }
+    }
+}
---
+++ KW24_25/Polymorphism.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILA23_M320_4.Semester.KW24_25
+{
+    internal class Polymorphism
+    {
+        static void Main(string[]args)
+        {
+            ZahlungsAbwicklung kreditkarte = new Kreditkartenabwicklung();
+
+            Zahlungsdienst z1 = new Zahlungsdienst(kreditkarte);
+
+            z1.ZahlefürBestellung(100);
+        }
+    }
+
+
+    public interface ZahlungsAbwicklung
+    {
+        void ZahleBetrag(decimal betrag);
+    }
+
+
+    public class Kreditkartenabwicklung : ZahlungsAbwicklung
+    {
+        public void ZahleBetrag(decimal betrag)
+        {
+            Console.WriteLine($"Kreditkartenzahlung {betrag} wird verarbeitet");
+        }
+    }
+
+
+    public class PayPalAbwicklung : ZahlungsAbwicklung
+    {
+        public void ZahleBetrag(decimal betrag)
+        {
+            Console.WriteLine($"Paypalabwicklung {betrag} wird verarbeitet");
+        }
+    }
+
+    public class Zahlungsdienst
+    {
+        private ZahlungsAbwicklung _zahlungsabwicklung;
+
+        public Zahlungsdienst(ZahlungsAbwicklung zahlungsAbwicklung)
+        {
+            this._zahlungsabwicklung = zahlungsAbwicklung;
+        }
+
+
+        public void ZahlefürBestellung(decimal betrag)
+        {
+            _zahlungsabwicklung.ZahleBetrag(betrag);
+        }
+    }
+}
